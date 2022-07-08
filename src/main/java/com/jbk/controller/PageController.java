db94227bd0d3e5843f0fe3a2ad3789b6a4fbcd7e
vaibhav anand sharma <vaibhav.mitujn777@gmail.com>
2022-07-08 17:33:20 +0530

    Ticket-02 Comment added in Page controller
    Ticket-02 Comment added in Page controller

--- src/main/java/com/jbk/controller/PageController.java
+++ src/main/java/com/jbk/controller/PageController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
-
+// This is just to direct navigate
 	@RequestMapping(value = "/")
 	public String loginPage()
 	{
